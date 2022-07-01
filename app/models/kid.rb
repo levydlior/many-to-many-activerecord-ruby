077200263a6402220bc3e2066d3ed4bf1fc3a363
Lior <levydlior@gmail.com>
2022-07-01 12:47:59 -0600

    finished all methods

--- app/models/kid.rb
+++ app/models/kid.rb
@@ -9,4 +9,17 @@ class Kid < ActiveRecord::Base
     end    
     Game.create(title: game_title, price: price, kid_id: self.id, store_id: store.id)
   end
+
+
+  def sid_broke_my_toy
+    game = self.games.order(price: :desc).first
+    game_to_destroy = Game.find(game.id)
+    game_to_destroy.destroy
+    puts "Sid, you're a bad person for destroying my #{game.title}!"
+  end
+
+  def self.spoiled_kid
+    all.max_by {|kid| kid.games.length}
+  end
+
 end
