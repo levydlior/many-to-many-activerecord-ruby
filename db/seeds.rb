d6753bc15aa44f2b15bf81117f8cf8ea34ac8a65
Lior <levydlior@gmail.com>
2022-07-01 10:36:51 -0600

    modles,seeds,migrations are built

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,9 @@
-# puts "Planting Seeds 🌱🌱🌱🌱"
+puts "Planting Seeds 🌱🌱🌱🌱"
 
-# 50.times do
-#   Kid.create(name: Faker::Name.first_name, age: rand(1...10))
-#   Store.create(name: Faker::Company.name, profit_per_year: rand(0...100000))
-#   Game.create(title: Faker::Game.title, price: rand(0..100), store_id: rand(0...50), kid_id: rand(0...50))
-# end
+50.times do
+  Kid.create(name: Faker::Name.first_name, age: rand(1...10))
+  Store.create(name: Faker::Company.name, profit_per_year: rand(0...100000))
+  Game.create(title: Faker::Game.title, price: rand(0..100), store_id: rand(0...50), kid_id: rand(0...50))
+end
 
-# puts "Done planting 🌱🌱🌱🌱"
+puts "Done planting 🌱🌱🌱🌱"
